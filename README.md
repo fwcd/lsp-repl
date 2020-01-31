7cbb12bc751ea618ea1eae66fd0e184828f111ea
fwcd <fwcdmail@gmail.com>
2020-01-31 19:06:19 +0100

    Implement basic request/response handling

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Language Server Protocol REPL
 An interactive environment that acts as a client to a [language server](https://microsoft.github.io/language-server-protocol/) and allows the user to easily perform/debug requests.
+
+## Try it
+Run `npm start -- path/to/languageserver`, then enter:
+
+```
+LSP> initialize({})
+```
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const process = require("process");
 const readline = require("readline");
 const rpc = require("vscode-jsonrpc");
 
+const flatMap = (f, arr) => arr.reduce((xs, x) => [...xs, ...f(x)], []);
+
 function main() {
     const argv = process.argv;
     if (argv.length == 3) {
@@ -17,20 +19,38 @@ function main() {
         const rl = readline.createInterface(process.stdin, process.stdout);
         rl.setPrompt("LSP> ");
         rl.prompt();
-        rl.on("line", line => {
-            if (line == "exit" || line == "quit") rl.close();
+        rl.on("line", async line => {
+            if (line == "exit" || line == "quit") {
+                rl.close();
+                return;
+            }
             const pattern = /(\w+)\((.*)\)/;
             const match = pattern.exec(line);
             
             if (match) {
-                // TODO
+                const method = match[1];
+                let params = flatMap(
+                    s => s ? [JSON.parse(s)] : [],
+                    match[2].split(/,\s*/)
+                );
+                console.log(`Sending request ${method} with params ${JSON.stringify(params)}...`);
+                
+                try {
+                    const response = await connection.sendRequest(method, ...params)
+                    console.log(JSON.stringify(response, null, 2));
+                } catch (e) {
+                    console.log(e);
+                }
             } else {
                 console.log("Please invoke a command! Example: name(argument1, argument2)");
             }
 
             rl.prompt();
         });
-
+        rl.on("close", () => {
+            serverProcess.kill("SIGINT");
+            process.exit(0);
+        });
     } else {
         console.log(`Usage: node ${argv[1]} [path to language server executable]`)
     }
